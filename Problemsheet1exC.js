20b8df414d33a6c7e3efb0b0e9948b321c96eec9
Nelson Almeida <96999074+nelsondca@users.noreply.github.com>
2023-09-29 18:16:02 +0100

    Update Problemsheet1exC.js

--- Problemsheet1exC.js
+++ Problemsheet1exC.js
@@ -1,6 +1,6 @@
 //taking 2 variables as parameters
 const sum = (a, b) => {
-    return(a+b)//the expcted return should be 2 variables added
+    return(a+b)//the expected return should be 2 variables added
   }
     x= sum(5,10)//creating a new variable to store the result of the function sum
-    console.log(x);
+    console.log(x);//displaying the result to the console
