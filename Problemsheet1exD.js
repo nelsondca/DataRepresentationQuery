ceccfc830936efd47e679c3b7c2b505adaa6479c
Nelson Almeida <96999074+nelsondca@users.noreply.github.com>
2023-09-29 18:17:46 +0100

    Update Problemsheet1exD.js

--- Problemsheet1exD.js
+++ Problemsheet1exD.js
@@ -2,14 +2,15 @@ const myArray = [25, 31, 42, 77];//declaring a variable
 
 const test = myArray.map((element)=>{//all the logic inside these brakets
 
-    if(element < 70){//Creating the condition
+    if(element < 70){//Creating the condition when each element inside the array is less than 70 gets multiplied by 2
 
     return element*2
 
     }else{
-        return element;//avoiding the undefined output
+        //or else to avoid the undefined output return the element itself
+        return element;
     }
 });
 
-console.log(test);//
+console.log(test);//outputting the test function
 
